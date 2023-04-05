56be49dd727d292965898e2b832d2b86ecacded3
Arpit Goyal <92arpit.goyal@gmail.com>
2023-04-06 03:02:56 +0530

    add support for gatsby to deliver index.html

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+const express = require('express');
 module.exports = {
   siteMetadata: {
     title: "Gatsby + Netlify CMS Starter",
@@ -88,4 +89,7 @@ module.exports = {
     }, // must be after other CSS plugins
     // "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
+  developMiddleware: (app) => {
+    app.use("/admin", express.static("public/admin"));
+  },
 };
